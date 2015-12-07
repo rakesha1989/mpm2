5d5b029b85187ab6dc9310718d36e65a0fb18eca
rakesha1989 <rakesh.qubik@gmail.com>
2015-12-07 13:03:21 +0530

    devise added

--- config/application.rb
+++ config/application.rb
@@ -22,5 +22,16 @@ module Mpm2
 
     # Do not swallow errors in after_commit/after_rollback callbacks.
     config.active_record.raise_in_transactional_callbacks = true
+
+    ActionMailer::Base.smtp_settings = { 
+   :address => "smtp.gmail.com",
+   :port => 587,
+   :domain => "gmail.com",
+   :authentication => "login",
+   :enable_starttls_auto => true,
+   :user_name => "rakesh.qubik@gmail.com",
+   :password => "civilice",
+   :openssl_verify_mode  => 'none'
+}
   end
 end
